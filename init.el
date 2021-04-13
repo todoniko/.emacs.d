5876fdea2460135401aff8b1e7e6049c564129ad
todoniko <nmtodorov@gmail.com>
2021-04-13 14:50:11 +0300

    [FIX] Clean up and refactor

--- init.el
+++ init.el
@@ -7,12 +7,14 @@
                      gcs-done)))
 
 ;; Minimal UI
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
-(menu-bar-mode   -1)
+(scroll-bar-mode   -1)
+(tool-bar-mode     -1)
+(tooltip-mode      -1)
+(menu-bar-mode     -1)
+(blink-cursor-mode -1)
 
 (setq inhibit-startup-message t
+      initial-scratch-message ""
       inhibit-splash-screen t)
 
 ;; Package configs
